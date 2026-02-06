ad1d11056fe3186209445ca85f599428d7018ad7
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 15:18:35 +0100

    Adjustment fuer die Zombiehorde

---
+++ ex01/Zombie.hpp
@@ -0,0 +1,24 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie {
+private:
+    std::string _name;
+
+public:
+	//Constructor
+	Zombie(std::string name);
+	// Constructor für Arrays!
+	Zombie();
+	// Setter für Name
+	void setName(std::string name);
+
+	~Zombie();
+	void announcement(void);
+};
+
+Zombie* zombieHorde(int N, std::string name);
+#endif
