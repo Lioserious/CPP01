fce1e4aa6f6614b2f6acd6f37559afaf8593c08c
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 16:47:19 +0100

    Weapon cpp imlementation

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,26 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:34:23 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/06 16:34:24 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/06 16:46:57 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+// Constructor
+Weapon::Weapon(std::string type) : type(type)
+{
+    
+}
+
+// Getter
+const std::string& Weapon::getType() const
+{
+    return type;
+}
+
+// Setter
+void Weapon::setType(std::string newType)
+{
+    type = newType;
+}
