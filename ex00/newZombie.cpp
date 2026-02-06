f347ea6f0e590cab65d2110995e37ae1e1ca95ee
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 14:52:04 +0100

    new Zombie allocates Zombie on heap

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,21 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 13:59:45 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/06 13:59:46 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/06 14:49:46 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+/*
+1. `new Zombie(name)` erstellt einen Zombie **auf dem Heap**
+2. Ruft den Constructor mit `name` auf
+3. Gibt die **Adresse** des neuen Zombies zurück
+4. Diese Adresse wird in `zombie` gespeichert
+*/
+Zombie* newZombie(std::string name)
+{
+    Zombie* zombie = new Zombie(name);
+    return zombie;
+}
 
