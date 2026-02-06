7f253ddc51e0df8882a747a86641727d4234e9be
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 14:12:36 +0100

    setup class with constructor and destructor

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -1,5 +1,19 @@
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string name;
+	public:
+		//Constructor
+		Zombie(std::string name);
+		//Deconstructor
+		~Zombie();
+		void announcement(void);
+};
 
 #endif
