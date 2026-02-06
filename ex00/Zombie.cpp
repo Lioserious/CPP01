5aa389c33b766b29e2e179ba6b15ad5df78c93fb
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 14:19:45 +0100

    deconstructor

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 13:52:33 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/06 14:16:34 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/06 14:19:22 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 Zombie::Zombie(std::string name) : name(name)
 {
     std::cout << name << " erstellt" << std::endl;
+}
+//deconstructor
+Zombie::~Zombie(void)
+{
+    std::cout << name << " zerstoerrt" << std::endl;
 }
