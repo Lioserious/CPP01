183c92c72fe363e05b5d654cf21db488752a248e
Leon Ihrig <lihrig@2-A-6.42heilbronn.de>
2026-02-06 16:22:19 +0100

    added main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 16:16:10 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/06 16:16:24 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/06 16:17:51 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,25 @@
 
 int main()
 {
-   
+    // Create string
+    std::string str = "HI THIS IS BRAIN";
     
+    // Create pointer
+    std::string* stringPTR = &str;
+    
+    // Create reference to the string
+    std::string& stringREF = str;
+    
+    // Print memory addresses
+    std::cout << "Memory address of str:       " << &str << std::endl;
+    std::cout << "Memory address in stringPTR: " << stringPTR << std::endl;
+    std::cout << "Memory address of stringREF: " << &stringREF << std::endl;
+    
+    std::cout << std::endl;
+    
+    // Print values
+    std::cout << "Value of str:        " << str << std::endl;
+    std::cout << "Value via stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value via stringREF: " << stringREF << std::endl;
     return 0;
 }
