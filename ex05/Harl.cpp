bc7731a2359fa379710d11bfcb5c3499e846ddf5
Leon Ihrig <lihrig@2-A-1.42heilbronn.de>
2026-02-08 10:35:04 +0100

    implemented Harl.cpp

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,54 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 10:29:48 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/08 10:29:49 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/08 10:33:05 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void Harl::debug(void)
+{
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void Harl::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+}
+
+void Harl::error(void)
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level)
+{
+    // Array von Level-Namen
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    
+    // Array von Function Pointers
+    void (Harl::*functions[4])(void) = {
+        &Harl::debug,
+        &Harl::info,
+        &Harl::warning,
+        &Harl::error
+    };
+    
+    // Finde und rufe die richtige Funktion auf
+    for (int i = 0; i < 4; i++)
+    {
+        if (level == levels[i])
+        {
+            (this->*functions[i])();
+            return;
+        }
+    }
+    
+    std::cout << "Unknown level: " << level << std::endl;
+}
