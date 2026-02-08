f7c7f4ee0b53732bd3dfe8c07608b06b3f971508
Leon Ihrig <lihrig@2-A-1.42heilbronn.de>
2026-02-08 11:01:39 +0100

    main function with test statements

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,30 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 10:28:51 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/08 10:28:52 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/08 10:41:29 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main()
+{
+    Harl harl;
+    
+    std::cout << "=== Testing DEBUG ===" << std::endl;
+    harl.complain("DEBUG");
+    
+    std::cout << "\n=== Testing INFO ===" << std::endl;
+    harl.complain("INFO");
+    
+    std::cout << "\n=== Testing WARNING ===" << std::endl;
+    harl.complain("WARNING");
+    
+    std::cout << "\n=== Testing ERROR ===" << std::endl;
+    harl.complain("ERROR");
+    
+    std::cout << "\n=== Testing Invalid ===" << std::endl;
+    harl.complain("INVALID");
+    
+    return 0;
+}
