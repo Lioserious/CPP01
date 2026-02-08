16520aa68fc24e68fd59819df9a61c5eab933a7c
Leon Ihrig <lihrig@2-A-1.42heilbronn.de>
2026-02-08 11:00:38 +0100

    Implementierung der Pointer to member function

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: lihrig <lihrig@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 10:29:48 by lihrig            #+#    #+#             */
-/*   Updated: 2026/02/08 10:33:05 by lihrig           ###   ########.fr       */
+/*   Updated: 2026/02/08 10:51:40 by lihrig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 void Harl::debug(void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+    std::cout << "Lecker Burger" << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << "Leckere Burger inklusive Bacon, der kostet Extra" << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years, whereas you started working here just last month." << std::endl;
+    std::cout << "Stammkunden, sollten extra Bacon bekommen" << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "Inakzeptable, ich moechte den manager sehen" << std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-    // Array von Level-Namen
+    // Array
     std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     
     // Array von Function Pointers
@@ -44,8 +44,7 @@ void Harl::complain(std::string level)
         &Harl::warning,
         &Harl::error
     };
-    
-    // Finde und rufe die richtige Funktion auf
+    // Finde und iteriere durch die Funktionen
     for (int i = 0; i < 4; i++)
     {
         if (level == levels[i])
@@ -54,6 +53,5 @@ void Harl::complain(std::string level)
             return;
         }
     }
-    
     std::cout << "Unknown level: " << level << std::endl;
 }
